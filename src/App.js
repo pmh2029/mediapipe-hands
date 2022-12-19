636a1d11288f2f7cac4018f8dafbd9fed64033ec
pmh <hieu.pm2029@gmail.com>
2022-12-20 01:40:49 +0700

    determine when the left or right hand is raised

--- src/App.js
+++ src/App.js
@@ -31,6 +31,41 @@ function App() {
       }
     }
     canvasCtx.restore();
+
+    if (results.multiHandedness[0] !== undefined) {
+      if (results.multiHandedness[0].label === 'Left'
+        && results.multiHandLandmarks[0] !== undefined
+        && results.multiHandLandmarks[0][4].x > results.multiHandLandmarks[0][3].x > results.multiHandLandmarks[0][2].x > results.multiHandLandmarks[0][1].x
+        && results.multiHandLandmarks[0][8].x > results.multiHandLandmarks[0][7].x > results.multiHandLandmarks[0][6].x > results.multiHandLandmarks[0][5].x
+        && results.multiHandLandmarks[0][12].x > results.multiHandLandmarks[0][11].x > results.multiHandLandmarks[0][10].x > results.multiHandLandmarks[0][9].x
+        && results.multiHandLandmarks[0][16].x > results.multiHandLandmarks[0][15].x > results.multiHandLandmarks[0][14].x > results.multiHandLandmarks[0][13].x
+        && results.multiHandLandmarks[0][20].x > results.multiHandLandmarks[0][19].x > results.multiHandLandmarks[0][18].x > results.multiHandLandmarks[0][17].x
+        && results.multiHandLandmarks[0][4].y < results.multiHandLandmarks[0][3].y < results.multiHandLandmarks[0][2].y < results.multiHandLandmarks[0][1].y
+        && results.multiHandLandmarks[0][8].y < results.multiHandLandmarks[0][7].y < results.multiHandLandmarks[0][6].y < results.multiHandLandmarks[0][5].y
+        && results.multiHandLandmarks[0][12].y < results.multiHandLandmarks[0][11].y < results.multiHandLandmarks[0][10].y < results.multiHandLandmarks[0][9].y
+        && results.multiHandLandmarks[0][16].y < results.multiHandLandmarks[0][15].y < results.multiHandLandmarks[0][14].y < results.multiHandLandmarks[0][13].y
+        && results.multiHandLandmarks[0][20].y < results.multiHandLandmarks[0][19].y < results.multiHandLandmarks[0][18].y < results.multiHandLandmarks[0][17].y
+      ) {
+        console.log("left hand is raise");
+      }
+
+      if (results.multiHandedness[0].label === 'Right'
+        && results.multiHandLandmarks[0] !== undefined
+        && results.multiHandLandmarks[0][4].x < results.multiHandLandmarks[0][3].x < results.multiHandLandmarks[0][2].x < results.multiHandLandmarks[0][1].x
+        && results.multiHandLandmarks[0][8].x < results.multiHandLandmarks[0][7].x < results.multiHandLandmarks[0][6].x < results.multiHandLandmarks[0][5].x
+        && results.multiHandLandmarks[0][12].x < results.multiHandLandmarks[0][11].x < results.multiHandLandmarks[0][10].x < results.multiHandLandmarks[0][9].x
+        && results.multiHandLandmarks[0][16].x < results.multiHandLandmarks[0][15].x < results.multiHandLandmarks[0][14].x < results.multiHandLandmarks[0][13].x
+        && results.multiHandLandmarks[0][20].x < results.multiHandLandmarks[0][19].x < results.multiHandLandmarks[0][18].x < results.multiHandLandmarks[0][17].x
+        && results.multiHandLandmarks[0][4].y < results.multiHandLandmarks[0][3].y < results.multiHandLandmarks[0][2].y < results.multiHandLandmarks[0][1].y
+        && results.multiHandLandmarks[0][8].y < results.multiHandLandmarks[0][7].y < results.multiHandLandmarks[0][6].y < results.multiHandLandmarks[0][5].y
+        && results.multiHandLandmarks[0][12].y < results.multiHandLandmarks[0][11].y < results.multiHandLandmarks[0][10].y < results.multiHandLandmarks[0][9].y
+        && results.multiHandLandmarks[0][16].y < results.multiHandLandmarks[0][15].y < results.multiHandLandmarks[0][14].y < results.multiHandLandmarks[0][13].y
+        && results.multiHandLandmarks[0][20].y < results.multiHandLandmarks[0][19].y < results.multiHandLandmarks[0][18].y < results.multiHandLandmarks[0][17].y
+      ) {
+        console.log("right hand is raise");
+      }
+    }
+  // console.log(results.multiHandLandmarks);
   }
 
   useEffect(() => {
@@ -43,7 +78,7 @@ function App() {
     hands.setOptions({
       maxNumHands: 2,
       modelComplexity: 1,
-      // selfieMode: false,
+      selfieMode: true,
       minDetectionConfidence: 0.5,
       minTrackingConfidence: 0.5
     });
